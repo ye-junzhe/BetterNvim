52444270af5319e27d61194766d357bfd6338ff4
McF4r <1206030024@qq.com>
2022-12-03 21:02:57 +0800

    illuminate: highlight under cursor

---
+++ lua/Junzhe/plugins/illuminate.lua
@@ -0,0 +1,12 @@
+vim.g.Illuminate_ftblacklist = { "alpha", "NvimTree" }
+vim.api.nvim_set_keymap("n", "<a-n>", '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', { noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<a-p>",
+	'<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>',
+	{ noremap = true }
+)
+
+vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
+vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
+vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
