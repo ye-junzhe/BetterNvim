afe7d42f9495c8d01f7a897458f219dcf68de4c2
Ye Junzhe <junzheyez@gmail.com>
2022-12-14 21:16:15 +0800

    Remove minimap autostart

--- lua/Junzhe/plugins/minimap.lua
+++ lua/Junzhe/plugins/minimap.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
 let g:minimap_width = 10
-let g:minimap_auto_start = 1
+" let g:minimap_auto_start = 1
 let g:minimap_highlight_search = 1
 let g:minimap_git_colors = 1
 let g:minimap_highlight_range = 1
