0c887347a969c83988eaadd950910cec625b9bd3
Ye Junzhe <junzheyez@gmail.com>
2023-09-28 15:42:53 +0800

    (1)Luasnip: tab => autocomplete & jump to next function parameter
    - cmp.confirm({ select = true }) this will automatically select the first item.
    - expand_or_locally_jumpable() instead of expand_or_jumpable() => prevent cursor from jumping back to the previous code block even it's far far away

--- lua/Junzhe/plugins/nvim-cmp.lua
+++ lua/Junzhe/plugins/nvim-cmp.lua
@@ -33,8 +33,15 @@ return {
                 ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<tab>"] = cmp.mapping.confirm({ select = true }),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.confirm({ select = true })
+                    elseif luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
