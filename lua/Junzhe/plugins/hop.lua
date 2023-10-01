5a9aa3400992a5d997568cee083c0454ee69146f
Ye Junzhe <junzheyez@gmail.com>
2023-10-01 12:07:49 +0800

    (1) the original hop.nvim is deprecated, change to its fork (2) Remove harpoon

--- lua/Junzhe/plugins/hop.lua
+++ lua/Junzhe/plugins/hop.lua
@@ -1,6 +1,6 @@
 return {
-    "phaazon/hop.nvim",
-    branch = "v2",
+    "smoka7/hop.nvim",
+    version = "*",
     config = function()
         -- you can configure Hop the way you like here; see :h hop-config
         require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
