2ff07a0aac0997fea083c76f64f639d9fc31e8ab
Ye Junzhe <junzheyez@gmail.com>
2023-10-04 12:47:14 +0800

    DO NOT AUTO FORMAT MY CODE !!!

--- lua/Junzhe/plugins/formatting.lua
+++ lua/Junzhe/plugins/formatting.lua
@@ -20,11 +20,11 @@ return {
                 lua = { "stylua" },
                 python = { "isort", "black" },
             },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            },
+            -- format_on_save = {
+            --     lsp_fallback = true,
+            --     async = false,
+            --     timeout_ms = 500,
+            -- },
         })
 
         vim.keymap.set({ "n", "v" }, "<leader>mp", function()
