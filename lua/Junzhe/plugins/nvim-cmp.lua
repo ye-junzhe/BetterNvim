7e30d6fbac5b254f03cab9b1288a08728879f4ee
Ye Junzhe <junzheyez@gmail.com>
2023-10-12 21:30:32 +0800

    Lazy.nvim event can't call multiple events in a single string

--- lua/Junzhe/plugins/nvim-cmp.lua
+++ lua/Junzhe/plugins/nvim-cmp.lua
@@ -1,6 +1,6 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter, BufEnter, BufReadPre, BufNewFile",
+    event = { "InsertEnter", "BufEnter", "BufReadPre", "BufNewFile" },
     dependencies = {
         "hrsh7th/cmp-buffer", -- source for text in buffer
         "hrsh7th/cmp-path", -- source for file system paths
