72e1201fc3fedf6cc64faf60480cdf3e51e95bc7
Ye Junzhe <junzhe@Bookworm.localdomain>
2023-11-08 19:33:45 +0800

    Made it work on linux

--- lua/Junzhe/plugins/nvim-tree.lua
+++ lua/Junzhe/plugins/nvim-tree.lua
@@ -93,8 +93,8 @@ return {
                 icons = {
                     glyphs = {
                         folder = {
-                            arrow_closed = "", -- arrow when folder is closed
-                            arrow_open = "", -- arrow when folder is open
+                            arrow_closed = "", -- arrow when folder is closed
+                            arrow_open = "", -- arrow when folder is open
                         },
                     },
                 },
