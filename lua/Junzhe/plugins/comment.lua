fd92e1e13448032c90270dd7860d1dce4e3610d6
Ye Junzhe <ye.junzhe@outlook.com>
2023-11-20 12:50:50 +0800

    context_commentstring is deprecated

--- lua/Junzhe/plugins/comment.lua
+++ lua/Junzhe/plugins/comment.lua
@@ -8,12 +8,18 @@ return {
         -- import comment plugin safely
         local comment = require("Comment")
 
-        local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+        local ts_context_commentstring = require("ts_context_commentstring")
+
+        ts_context_commentstring.setup({
+            enable_autocmd = false,
+        })
+
+        vim.g.skip_ts_context_commentstring_module = true
 
         -- enable comment
         comment.setup({
             -- for commenting tsx and jsx files
-            pre_hook = ts_context_commentstring.create_pre_hook(),
+            pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
         })
     end,
 }
--- lua/Junzhe/plugins/nvim-treesitter.lua
+++ lua/Junzhe/plugins/nvim-treesitter.lua
@@ -61,11 +61,6 @@ return {
                     node_decremental = "<bs>",
                 },
             },
-            -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-            context_commentstring = {
-                enable = true,
-                enable_autocmd = false,
-            },
             -- auto install above language parsers
             auto_install = true,
         })
