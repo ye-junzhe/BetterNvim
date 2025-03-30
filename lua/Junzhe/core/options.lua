8f738d1464aee694c0f65b9f4b57eec4482fbcea
Ye Junzhe <ye.junzhe@outlook.com>
2025-03-30 20:40:43 +0800

    Add more tweaks to support Linux

--- lua/Junzhe/core/options.lua
+++ lua/Junzhe/core/options.lua
@@ -40,7 +40,6 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
 
-vim.g.tagbar_ctags_bin = "/opt/homebrew/Cellar/universal-ctags/p6.0.20230423.0/bin/ctags"
 
 -- turn off swapfile
 opt.swapfile = false
